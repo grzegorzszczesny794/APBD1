5433618225ea17c3ee781c66591b27e5d4cd0261
s27881 <s27881@pjwstk.edu.pl>
2025-03-02 16:25:02 +0100

    Modyfikacja druga

--- tt/Program.cs
+++ tt/Program.cs
@@ -4,6 +4,7 @@ public sealed class Program
     public static async Task Main(string[] args)
     {
         Console.WriteLine("Modyfikacja pierwsza.");
-    
+        Console.WriteLine("Modyfikacja druga.");
+
     }
 }
